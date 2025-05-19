85fabc557f83119e517280a2cec837132f4b7b61
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-19 17:25:05 -0300

    feat: added Category and Product model in admin

--- api/categories/admin.py
+++ api/categories/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from categories.models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+admin.site.register(Category, CategoryAdmin)
--- api/products/admin.py
+++ api/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+
+
+admin.site.register(Product, ProductAdmin)
