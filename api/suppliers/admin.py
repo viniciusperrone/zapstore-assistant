6b9ec7a3309afca8ee1e74806f2e6ae867c5e5fa
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-19 19:13:09 -0300

    feat: registered Supplier model in admin

--- api/suppliers/admin.py
+++ api/suppliers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from suppliers.models import Supplier
+
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(Supplier, SupplierAdmin)
