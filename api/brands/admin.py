ba614bb10ff1910f6ab483ba79ecdd4f5764eefe
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-19 17:04:04 -0300

    feat: registered BrandAdmin

--- api/brands/admin.py
+++ api/brands/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from brands.models import Brand
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+admin.site.register(Brand, BrandAdmin)
