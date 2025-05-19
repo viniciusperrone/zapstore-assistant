56ef5c963f3c2f619359b56d40f4822e6bd53a5d
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-19 18:38:30 -0300

    feat: added ProductListCreateAPIView ProductRetrieveUpdateDestroyAPIView

--- api/products/views.py
+++ api/products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from products.models import Product
+from products.serializers import ProductSerializer
+
+
+class ProductListCreateAPIView(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
