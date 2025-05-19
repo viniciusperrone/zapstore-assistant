06784a73384564495398f8a905c27843096594ec
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-19 18:56:53 -0300

    feat: created InventoryListCreateAPIView and InventoryRetrieveUpdateDestroyAPIView

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from inventory.models import Inflow
+from inventory.serializers import InflowSerializer
+
+
+class InventoryListCreateAPIView(ListCreateAPIView):
+    queryset = Inflow.objects.all()
+    serializer_class = InflowSerializer
+
+
+class InventoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Inflow.objects.all()
+    serializer_class = InflowSerializer
