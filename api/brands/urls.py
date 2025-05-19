335df52cc4865e737f784ceb680c5147e371e60d
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-19 18:03:35 -0300

    feat: created BrandRetrieveUpdateDestroyAPIView

--- api/brands/urls.py
+++ api/brands/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from utils.api import BASIC_API_URL
-from brands.views import BrandListCreateAPIView
+from brands.views import BrandListCreateAPIView, BrandRetrieveUpdateDestroyAPIView
+
 
 urlpatterns = [
-    path(f'{BASIC_API_URL}/brand', BrandListCreateAPIView.as_view(), name='brand-create-list-view')
+    path(f'{BASIC_API_URL}/brand', BrandListCreateAPIView.as_view(), name='brand-create-list-api-view'),
+    path(f'{BASIC_API_URL}/brand/<int:pk>/', BrandRetrieveUpdateDestroyAPIView.as_view(), name='brand-detail-api-view'),
 ]
--- api/brands/views.py
+++ api/brands/views.py
@@ -1,9 +1,14 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from brands.models import Brand
 from brands.serializers import BrandSerializer
 
 
 class BrandListCreateAPIView(ListCreateAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+
+class BrandRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
