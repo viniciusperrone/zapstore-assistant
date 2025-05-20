75e332da89e8d8d12bdb315e11618498f5b3ed03
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-20 17:45:36 -0300

    feat: added SalesListCreateAPIView

--- api/sales/views.py
+++ api/sales/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+from sales.models import Sales
+from sales.serializers import SalesSerializer
+
+
+class SalesListCreateAPIView(ListCreateAPIView):
+    queryset = Sales.objects.all()
+    serializer_class = SalesSerializer
