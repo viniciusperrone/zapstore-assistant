ebe1f1adf29f1ed0a2cd93a40e7c6fc3dcc84e6f
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-22 15:03:13 -0300

    feat: added EVOLUTION_INSTANCE_NAME

--- chatbot-service/app.py
+++ chatbot-service/app.py
@@ -1,12 +1,42 @@
 from fastapi import FastAPI, Request
-from models.message import Message
+
+from services.evolution import Evolution
 
 
 app = FastAPI()
 
 @app.post("/chatbot/assistant")
 async def root(request: Request):
-    print(request.json())
+    data = await request.json()
+
+    chat_id = data['data']['key']['remoteJid']
+    from_me = data['data']['key']['fromMe']
+    author = data['data']['pushName']
+
+    is_group_message = '@g.us' in chat_id
+
+    number_contact = None if is_group_message else chat_id.split('@s.')[0]
+
+    if from_me or is_group_message:
+        return {'status': 'Success', 'message': 'Message ignored'}
+
+    if number_contact:
+        evolution = Evolution()
+
+        await evolution.send_message(
+            message=f'Olá, {author}',
+            number_contact=chat_id
+        )
 
     return {'status': 'ok'}
 
+@app.post("/chatbot/test")
+async def test_chatbot(request: Request):
+    evolution = Evolution()
+
+    evolution.send_message(
+        message=f'Olá, Vinicius',
+        number_contact='554174017213'
+    )
+
+    return {'status': 'ok'}
--- chatbot-service/services/evolution.py
+++ chatbot-service/services/evolution.py
@@ -2,7 +2,8 @@ import requests
 
 from config.environments import (
     EVOLUTION_API_URL,
-    AUTHENTICATION_API_KEY
+    AUTHENTICATION_API_KEY,
+    EVOLUTION_INSTANCE_NAME
 )
 
 
@@ -11,9 +12,10 @@ class Evolution:
     def __init__(self):
         self.__api_url = EVOLUTION_API_URL
         self.__api_key = AUTHENTICATION_API_KEY
+        self.__evolution_instance = EVOLUTION_INSTANCE_NAME
 
     def send_message(self, number_contact, message):
-        url = f'{self.__api_url}/message/sendText/ZapStore-Assistant'
+        url = f'{self.__api_url}/message/sendText/{self.__evolution_instance}'
 
         headers = {
             'apikey': self.__api_key,
