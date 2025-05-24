d4dbb456187426d3bd96870a0effa4aeab16cf02
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-23 23:13:11 -0300

    feat: created PROMPT_DETECT_TYPE_MESSAGE

--- chatbot-service/utils/prompts.py
+++ chatbot-service/utils/prompts.py
@@ -1,4 +1,25 @@
-PROMPT_AGENT_SYSTEM = """
-    Você é um agente virtual especialista em gestão de produtos da ZapStore (loja de periféricos de tecnologia).
-    Bem como tirar dúvidas e sugestões para os clientes.
+PROMPT_RESUME_AGENT_SYSTEM = """
+    Você é um agente virtual especialista em gestão de produtos
+    da ZapStore (loja de periféricos de tecnologia).
 """
+
+PROMPT_DETECT_TYPE_MESSAGE = """
+    {{resume}}
+
+    Indetifique a intenção da mensagem, com os seguintes tipos e exemplos:
+        - 'FIRST_INTERACTION': Comprimento (exemplo: Olá, tudo bem? Boa noite)
+        - 'PRODUCTS': Pergunta sobre qualquer produtos, sem falar especificamente.
+        - 'SPECIFIC_PRODUCT': Algum produto (exemplo: Notebook Dell Inspiron)
+        - 'INFORMATION_ABOUT_BRANDS': Pergunta sobre marcas (exemplo: Tem a marca da Logitech?; Quais marcas vocês vendem?)
+        - 'CHEAPER_PRODUCTS': Estatísticas sobre produtos (exemplo: Produtos mais baratos, Produtos mais vendidos)
+        - 'UNKNOWN': Mensagem sem contexto com a ZapStore (exemplo: Já assistiu o filme da Marvel?)
+
+    E depois retorne um prompt customizado para uma nova chamada para o sistema, e não para o humano (Obs: se o tipo for 'FIRST_INTERACTION' ou 'UNKNOWN' deve ser nulo).
+
+    Sua resposta deve ser um json, exemplo:
+
+    {
+        "type": "FIRST_INTERACTION",
+        "output": "Exemplo de prompt de saída"
+    }
+""".replace('{{resume}}', PROMPT_RESUME_AGENT_SYSTEM)
