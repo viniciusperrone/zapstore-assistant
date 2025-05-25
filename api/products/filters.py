8d91ecfe36ceeeeda88ddaea65c240ec7bc7a75d
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-25 19:38:11 -0300

    feat: updated ProductFilter with adding brand and category fields

--- api/products/filters.py
+++ api/products/filters.py
@@ -4,10 +4,12 @@ from products.models import Product
 
 
 class ProductFilter(filters.FilterSet):
+    title = filters.CharFilter(field_name='title', lookup_expr='icontains')
+    description = filters.CharFilter(field_name='description', lookup_expr='icontains')
+    brand = filters.CharFilter(field_name='brand__name', lookup_expr='icontains')
+    category = filters.CharFilter(field_name='category__name', lookup_expr='icontains')
+
 
     class Meta:
         model = Product
-        fields = {
-            'title': ['icontains'],
-            'description': ['icontains']
-        }
+        fields = ['title', 'description', 'brand', 'category']
