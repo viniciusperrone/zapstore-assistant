673ae8584eb26b0a38203f544ab66039d7af44c6
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-25 19:45:13 -0300

    feat: applied filters in BrandListCreateAPIView

--- api/brands/views.py
+++ api/brands/views.py
@@ -1,12 +1,16 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django_filters import rest_framework as filters
 
 from brands.models import Brand
 from brands.serializers import BrandSerializer
+from brands.filters import BrandFilter
 
 
 class BrandListCreateAPIView(ListCreateAPIView):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = BrandFilter
 
 
 class BrandRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
