7e1b5f47885e19a2e7fc81ee202d5716be9d7be7
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-26 10:30:55 -0300

    feat: created CategoryFilter

---
+++ api/categories/filters.py
@@ -0,0 +1,21 @@
+from django_filters import rest_framework as filters
+from django.db.models import Q
+
+from categories.models import Category
+
+
+class CategoryFilter(filters.FilterSet):
+    search = filters.CharFilter(method='filter_search')
+
+    def filter_search(self, queryset, name, value):
+        return queryset.filter(
+            Q(name__icontains=value) | Q(description__icontains=value)
+        )
+
+
+    class Meta:
+        model = Category
+        fields = {
+            'name': ['icontains'],
+            'description': ['icontains']
+        }
