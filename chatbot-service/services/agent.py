4ecc375e5941a3f0934597e4bd2c825166c4a0b0
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-25 21:42:42 -0300

    feat: improving invoke in Agent

--- chatbot-service/services/agent.py
+++ chatbot-service/services/agent.py
@@ -1,30 +1,26 @@
 import json
 import requests
+from typing import Dict, Any
 
 from openai import OpenAI
+from services.catalog import Catalog
 
-from config.environments import API_URL, OPENAI_API_KEY
+from config.environments import OPENAI_API_KEY
 from utils.kind_message import MessageType
 from utils.prompts import (
     PROMPT_RESUME_AGENT_SYSTEM,
     PROMPT_DETECT_TYPE_MESSAGE
 )
-
+from utils.messages import OUT_OF_SCOPE
 
 class Agent:
 
     def __init__(self) -> None:
-        self.__catalog_url = f"{API_URL}/api/v1/all_data"
+        self.__catalog = Catalog()
         self.__client = OpenAI(
             api_key=OPENAI_API_KEY
         )
 
-    def load_catalog_data(self):
-        response = requests.get(self.__catalog_url)
-
-        data = response
-
-        return data.json()
 
     def __invoke_llm(self, system_prompt, user_prompt):
         response = self.__client.chat.completions.create(
@@ -43,7 +39,7 @@ class Agent:
 
         return response.choices[0].message.content
 
-    def detect_type_message(self, message):
+    def detect_type_message(self, message) -> Dict[Any, Any]:
         response = self.__invoke_llm(user_prompt=message, system_prompt=PROMPT_DETECT_TYPE_MESSAGE)
 
         try:
@@ -52,11 +48,100 @@ class Agent:
         except json.JSONDecodeError:
 
             return {
-                'type': MessageType.UNKNOWN,
+                'type': MessageType.UNKNOWN.value,
+                'filters': None,
                 'output': None
             }
 
-    def invoke(self, received_message: str, context: list[str] | None = None) -> None:
+    def invoke(self, message: str, data: dict) -> str:
+        message_type = MessageType(data.get('type', MessageType.UNKNOWN.value))
+        filters = data.get('filters', None)
+        output_prompt = data.get('output_prompt', None)
+        catalog_data: str = None
+        messages_prompt = []
+
+        if message_type == MessageType.PRODUCT:
+            catalog_json = self.__catalog.get_products(filters=filters)
+
+            catalog_data = json.dumps(catalog_json)
+
+        if message_type == MessageType.BRAND:
+            catalog_json = self.__catalog.get_products(filters=filters)
+
+            catalog_data = json.dumps(catalog_json)
+
+        if message_type == MessageType.CATEGORY:
+            catalog_json = self.__catalog.get_categories(filters=filters)
+
+            catalog_data = json.dumps(catalog_json)
+
+        if message_type == MessageType.SUPPLIER:
+            catalog_json = self.__catalog.get_suppliers(filters=filters)
+
+            catalog_data = json.dumps(catalog_json)
+
+        if message_type == MessageType.INVENTORY:
+            catalog_json = self.__catalog.get_inventory(filters=filters)
+
+            catalog_data = json.dumps(catalog_json)
+
+        if message_type == MessageType.SALE:
+            catalog_json = self.__catalog.get_sales(filters=filters)
+
+            catalog_data = json.dumps(catalog_json)
+
+        if message_type == MessageType.UNKNOWN:
+            return OUT_OF_SCOPE
+
+        system_prompt = f"{output_prompt}\n\nDados (a serem analisados): {catalog_data}"
+
+        messages_prompt.append({
+            'role': 'system',
+            'content': system_prompt
+        })
+
+        messages_prompt.append({
+            'role': 'user',
+            'content': message
+        })
+
+        response = self.__client.chat.completions.create(
+            model='gpt-3.5-turbo',
+            messages=messages_prompt
+        )
+
+        return response
+
+
+    def process_intent(self, message: str, intent_type: str):
+        catalog_data = self.load_catalog_data()
+
+        if MessageType(intent_type) == MessageType.PRODUCTS:
+            prompt = f"Liste os produtos disponíveis com base nessa base de dados:\n\n{catalog_data}"
+
+        elif MessageType(intent_type) == MessageType.SPECIFIC_PRODUCT:
+            prompt = f"Com base na seguinte base de dados:\n\n{catalog_data}\n\nResponda à pergunta do usuário: {message}"
+
+        elif MessageType(intent_type) == MessageType.INFORMATION_ABOUT_BRANDS:
+            brands = list({item["brand"] for item in catalog_data})  # Extrai marcas únicas
+            prompt = f"Estas são as marcas disponíveis: {', '.join(brands)}.\nResponda de forma útil à pergunta: {message}"
+
+        elif MessageType(intent_type) == MessageType.CHEAPER_PRODUCTS:
+            sorted_data = sorted(catalog_data, key=lambda x: x["price"])
+            cheapest = sorted_data[:5]
+            prompt = f"Estes são os 5 produtos mais baratos:\n\n{cheapest}\n\nDê uma resposta amigável ao usuário."
+
+        else:
+            prompt = message
+
+        return self.__invoke_llm(system_prompt=PROMPT_RESUME_AGENT_SYSTEM, user_prompt=prompt)
+
+    def invokedede(
+        self,
+        received_message: str,
+        intent_type: str | None = None,
+        context: list[str] | None = None,
+    ) -> None:
         messages = [
             {
                 'role': 'system',
