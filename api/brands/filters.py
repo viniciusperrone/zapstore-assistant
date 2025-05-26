51ca8f8dfdbb8609f6992e9bfedd4d11e4a46913
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-26 10:28:38 -0300

    refactor: improving BrandFilter

--- api/brands/filters.py
+++ api/brands/filters.py
@@ -4,8 +4,9 @@ from brands.models import Brand
 
 
 class BrandFilter(filters.FilterSet):
-    name = filters.CharFilter(field_name='name', lookup_expr='icontains')
 
     class Meta:
         model = Brand
-        fields = ['name']
+        fields = {
+            'name': ['icontains']
+        }
