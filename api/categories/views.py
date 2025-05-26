a02cb49f7d68114739c7392fe035e973ce11c592
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-26 11:26:33 -0300

    feat: applied filters in categories views

--- api/categories/views.py
+++ api/categories/views.py
@@ -1,12 +1,16 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django_filters import rest_framework as filters
 
 from categories.models import Category
 from categories.serializers import CategorySerializer
+from categories.filters import CategoryFilter
 
 
 class CategoryListCreateAPIView(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = CategoryFilter
 
 class CategoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
