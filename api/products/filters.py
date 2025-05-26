b796c9211f535041cf71b988f88435f47d3e3852
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-25 22:25:28 -0300

    feat: added search field in product filters

--- api/products/filters.py
+++ api/products/filters.py
@@ -1,15 +1,22 @@
 from django_filters import rest_framework as filters
+from django.db.models import Q
 
 from products.models import Product
 
 
 class ProductFilter(filters.FilterSet):
-    title = filters.CharFilter(field_name='title', lookup_expr='icontains')
-    description = filters.CharFilter(field_name='description', lookup_expr='icontains')
-    brand = filters.CharFilter(field_name='brand__name', lookup_expr='icontains')
-    category = filters.CharFilter(field_name='category__name', lookup_expr='icontains')
+    search = filters.CharFilter(method='filter_search')
 
+    def filter_search(self, queryset, name, value):
+        return queryset.filter(
+            Q(title__icontains=value) | Q(description__icontains=value)
+        )
 
     class Meta:
         model = Product
-        fields = ['title', 'description', 'brand', 'category']
+        fields = {
+            'title': ['icontains'],
+            'description': ['icontains'],
+            'brand__name': ['icontains'],
+            'category__name': ['icontains'],
+        }
