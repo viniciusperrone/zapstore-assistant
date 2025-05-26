3f35259a43541f54f4fc0c53bf181293478542ba
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-26 11:27:57 -0300

    feat: added search filter field in BrandFilter

--- api/brands/filters.py
+++ api/brands/filters.py
@@ -1,9 +1,16 @@
+from django.db.models import Q
 from django_filters import rest_framework as filters
 
 from brands.models import Brand
 
 
 class BrandFilter(filters.FilterSet):
+    search = filters.CharFilter(method='filter_search')
+
+    def filter_search(self, queryset, name, value):
+        return queryset.filter(
+            Q(name__icontains=value) | Q(description__icontains=value)
+        )
 
     class Meta:
         model = Brand
