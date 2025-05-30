208077b252ef9e4b5f30a427db4ea324a8f1801b
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-29 23:24:59 -0300

    fix: implemented custom migration 002_sales_customer for existing old records

--- api/sales/models.py
+++ api/sales/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 from products.models import Product
+from customers.models import Customer
 
 
 class Sales(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='sales')
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT, related_name='sales')
 
     quantity = models.IntegerField(default=1)
     description = models.TextField()
