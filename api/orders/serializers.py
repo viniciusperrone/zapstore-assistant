eb1ab5054b41d2c8766b52f0fdc42730111702cd
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-30 23:23:17 -0300

    feat: created OrderSerializer and OrderItemSerializer

---
+++ api/orders/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+
+from orders.models import Order, OrderItem
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'customer', 'expiration_time', 'status', 'created_at', 'updated_at']
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['order', 'product', 'quantity']
