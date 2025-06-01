d78a1149d28aae7d20e4f5ac0f285dae991c9f72
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-05-31 22:04:38 -0300

    fix: adjusted OrderSerializer

--- api/orders/serializers.py
+++ api/orders/serializers.py
@@ -6,7 +6,8 @@ from orders.models import Order, OrderItem
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ['id', 'customer', 'expiration_time', 'status', 'created_at', 'updated_at']
+        fields = ['id', 'customer', 'status', 'created_at', 'updated_at']
+
 
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
