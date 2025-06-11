67aa4afe58b4fe634b7b71fd2501d651d885945f
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-06-11 18:02:00 -0300

    chore: api services containerized

--- api/orders/models.py
+++ api/orders/models.py
@@ -54,6 +54,7 @@ class Order(models.Model):
     def __str__(self):
         return f'Order #{self.id} - {self.customer.name}'
 
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
