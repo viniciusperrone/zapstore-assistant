39d1f25f86c1c118d7c000e031b7b2f41cef39e4
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-06-11 18:33:31 -0300

    chore: updated database

--- api/core/settings.py
+++ api/core/settings.py
@@ -70,18 +70,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'mydatabase',
-    #     'USER': 'myuser',
-    #     'PASSWORD': 'mypassword',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432',
-    # }
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
 }
 
 
